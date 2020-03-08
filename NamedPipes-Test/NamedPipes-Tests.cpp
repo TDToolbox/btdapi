ae4544a58edd8f2c4546f6d132ebe5b301d9d7e2
Danny Parker <ben@webzone.com.au>
2020-03-09 00:01:38 +1030

    Add multi architecture tests, fix x86 g3logger libs

---
+++ NamedPipes-Test/NamedPipes-Tests.cpp
@@ -0,0 +1,13 @@
+#include "pch.hpp"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NamedPipesTest {
+TEST_CLASS(NamedPipesMainTests){public :
+
+    TEST_METHOD(Test){Assert::AreEqual(0, 0);
+}
+}
+;
+}
