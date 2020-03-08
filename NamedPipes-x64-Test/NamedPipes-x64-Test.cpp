193bf15ff6700bef4a1c683dbcd9f94ec39f210c
Danny Parker <ben@webzone.com.au>
2020-03-08 22:50:47 +1030

    Fix g3log libraries, add test MSTest test

---
+++ NamedPipes-x64-Test/NamedPipes-x64-Test.cpp
@@ -0,0 +1,15 @@
+#include "pch.hpp"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NamedPipesx64Test
+{
+	TEST_CLASS(NamedPipesx64Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1){Assert::AreEqual(0, 0);
+}
+	};
+}
